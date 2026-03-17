f26a8b4dc475ab669673a5e1795a721a5449c3e2
Hiren Khunt <hirenkhunt101@gmail.com>
2026-03-17 16:12:05 +0530

    Backend url changes

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  apiUrl: 'http://localhost:3002/hotelpro/',
+  apiUrl: 'http://localhost:3003/hotelpro/',
   storageKey: 'userInfo',
   crypto_encryption_Key:
     'd9c18372bd8cc98a611fac4553f57bd4d6d9934c7ac113e087eae0cc3916af2f',
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  apiUrl: 'https://hotelpro-deploy-backend-git-main-hirenkhunt101s-projects.vercel.app/',
+  apiUrl: 'https://hotelpro-deploy-backend.vercel.app/hotelpro/',
   storageKey: 'userInfo',
   crypto_encryption_Key:
     'd9c18372bd8cc98a611fac4553f57bd4d6d9934c7ac113e087eae0cc3916af2f',
